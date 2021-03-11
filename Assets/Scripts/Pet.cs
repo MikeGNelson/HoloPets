7abf5c3f4ec47d6496331a4bb6365314dce63de9
Michael Nelson <nelso430@purdue.edu>
2021-03-11 17:44:54 -0500

    Voice commands

---
+++ Assets/Scripts/Pet.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pet : MonoBehaviour
+{
+    public Transform target;
+    public bool isReturning = false;
+    private float speed = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isReturning)
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+            if(Vector3.Distance( transform.position,target.position) <= .5f)
+            {
+                isReturning = false;
+            }
+        }
+        
+    }
+}
---
+++ Assets/Scripts/SpeechCommands.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeechCommands : MonoBehaviour
+{
+    public Transform ball;
+    public GameObject fox;
+    public Renderer foxRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            ChangeColor();
+        }
+    }
+
+    public void Fetch()
+    {
+
+    }
+
+    public void Return()
+    {
+        fox.GetComponent<Pet>().isReturning = true;
+    }
+
+    public void ChangeColor()
+    {
+        
+        foxRenderer.material.color = new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f));
+    }
+}
