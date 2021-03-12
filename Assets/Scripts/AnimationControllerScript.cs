69a8fec2233d657fb2cc13227de256a5c29e2015
heyimglory <jlps91060@gmail.com>
2021-03-12 11:14:08 -0500

    Animation for tricks and moving

---
+++ Assets/Scripts/AnimationControllerScript.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationControllerScript : MonoBehaviour
+{
+    // -------------------------------------
+    private float BORED_THRESHOLD = 10.0f;
+    // -------------------------------------
+
+    private Animator selfAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        selfAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.I))
+        {
+            if (selfAnimator.GetFloat("VSpeed") < 1.0f)
+                selfAnimator.SetFloat("VSpeed", selfAnimator.GetFloat("VSpeed") + 0.1f);
+            selfAnimator.SetFloat("Bored", 0.0f);
+        }
+        else if (Input.GetKey(KeyCode.K))
+        {
+            if (selfAnimator.GetFloat("VSpeed") > -1.0f)
+                selfAnimator.SetFloat("VSpeed", selfAnimator.GetFloat("VSpeed") - 0.1f);
+            selfAnimator.SetFloat("Bored", 0.0f);
+        }
+        else
+        {
+            if (selfAnimator.GetFloat("VSpeed") > 0.1f)
+                selfAnimator.SetFloat("VSpeed", selfAnimator.GetFloat("VSpeed") - 0.1f);
+            else if (selfAnimator.GetFloat("VSpeed") < -0.1f)
+                selfAnimator.SetFloat("VSpeed", selfAnimator.GetFloat("VSpeed") + 0.1f);
+        }
+
+        if (Input.GetKey(KeyCode.L))
+        {
+            if (selfAnimator.GetFloat("HSpeed") < 1.0f)
+                selfAnimator.SetFloat("HSpeed", selfAnimator.GetFloat("HSpeed") + 0.1f);
+            selfAnimator.SetFloat("Bored", 0.0f);
+        }
+        else if (Input.GetKey(KeyCode.J))
+        {
+            if (selfAnimator.GetFloat("HSpeed") > -1.0f)
+                selfAnimator.SetFloat("HSpeed", selfAnimator.GetFloat("HSpeed") - 0.1f);
+            selfAnimator.SetFloat("Bored", 0.0f);
+        }
+        else
+        {
+            if (selfAnimator.GetFloat("HSpeed") > 0.1f)
+                selfAnimator.SetFloat("HSpeed", selfAnimator.GetFloat("HSpeed") - 0.1f);
+            else if (selfAnimator.GetFloat("HSpeed") < -0.1f)
+                selfAnimator.SetFloat("HSpeed", selfAnimator.GetFloat("HSpeed") + 0.1f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            selfAnimator.SetBool("Jumping", true);
+            Invoke("StopJumping", 0.1f);
+            selfAnimator.SetFloat("Bored", 0.0f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            selfAnimator.SetBool("SittingChange", true);
+            Invoke("StopSittingChange", 0.1f);
+            selfAnimator.SetFloat("Bored", 0.0f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            selfAnimator.SetBool("FallingLeft", true);
+            Invoke("StopFallingLeft", 0.1f);
+            selfAnimator.SetFloat("Bored", 0.0f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            selfAnimator.SetBool("ChaseTail", true);
+            Invoke("StopChaseTail", 0.1f);
+            selfAnimator.SetFloat("Bored", 0.0f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            selfAnimator.SetBool("Roll", true);
+            Invoke("StopRoll", 0.1f);
+            selfAnimator.SetFloat("Bored", 0.0f);
+        }
+
+        if (selfAnimator.GetFloat("Bored") < BORED_THRESHOLD)
+        {
+            selfAnimator.SetFloat("Bored", selfAnimator.GetFloat("Bored") + Time.deltaTime);
+        }
+    }
+
+    void StopJumping()
+    {
+        selfAnimator.SetBool("Jumping", false);
+    }
+    void StopSittingChange()
+    {
+        selfAnimator.SetBool("SittingChange", false);
+    }
+    void StopFallingLeft()
+    {
+        selfAnimator.SetBool("FallingLeft", false);
+    }
+    void StopChaseTail()
+    {
+        selfAnimator.SetBool("ChaseTail", false);
+    }
+    void StopRoll()
+    {
+        selfAnimator.SetBool("Roll", false);
+    }
+}
